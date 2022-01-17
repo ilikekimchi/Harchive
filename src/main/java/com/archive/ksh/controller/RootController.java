d454f6113d7a518f3b76ca8aa35fe508b599b1c0
KSH <gimseonghyun5052@gmail.com>
2022-01-17 20:49:53 +0900

    first test commit

---
+++ src/main/java/com/archive/ksh/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.archive.ksh.controller;
+
+import java.rmi.server.RMIClassLoader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+		@RequestMapping()
+}
